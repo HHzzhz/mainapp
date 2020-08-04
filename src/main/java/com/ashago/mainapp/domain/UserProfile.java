ca8146eb0433acb0fa00b4a7207b88241de130b6
huazheng <huazheng@lattebank.com>
2020-08-05 00:27:11 +0800

    fix

--- src/main/java/com/ashago/mainapp/domain/UserProfile.java
+++ src/main/java/com/ashago/mainapp/domain/UserProfile.java
@@ -1,5 +1,6 @@
 package com.ashago.mainapp.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +12,8 @@ import javax.persistence.Id;
 @Entity
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserProfile {
 
     @Id
