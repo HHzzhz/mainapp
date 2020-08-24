260696ef3036bab0dc4a47a8dfb59a92fa2e4f9c
huazheng <huazheng@lattebank.com>
2020-08-24 17:33:16 +0800

    fix

---
+++ src/main/java/com/ashago/mainapp/resp/CommentResp.java
@@ -0,0 +1,26 @@
+package com.ashago.mainapp.resp;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class CommentResp {
+    //user
+    private String userId;
+    private String avatar;
+    private String author;
+    //blog
+    private String blogId;
+    private String blogTitle;
+    //comment
+    private LocalDateTime postAt;
+    private String content;
+
+}
