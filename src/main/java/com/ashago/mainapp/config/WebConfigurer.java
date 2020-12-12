147d68ed8ecc669f7bb350376d2dcbf544c0bc1e
huazheng <huazheng@lattebank.com>
2020-12-12 17:51:34 +0800

    cors fix

--- src/main/java/com/ashago/mainapp/config/WebConfigurer.java
+++ src/main/java/com/ashago/mainapp/config/WebConfigurer.java
@@ -24,6 +24,8 @@ public class WebConfigurer implements WebMvcConfigurer {
                         "http://ashago-api.cc2dbe1fd91f042528f96dc27c2dba5fe.cn-zhangjiakou.alicontainer.com",
                         "http://ashago.cc2dbe1fd91f042528f96dc27c2dba5fe.cn-zhangjiakou.alicontainer.com",
                         "http://www.ashago.com",
-                        "http://api.ashago.com");
+                        "http://api.ashago.com",
+                        "https://www.ashago.com",
+                        "https://api.ashago.com");
     }
 }
