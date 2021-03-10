4846ddbb43ce93802df5ffae3448e359d8cc31e4
huazheng <huazheng@lattebank.com>
2021-03-10 16:42:35 +0800

    service

---
+++ src/main/java/com/ashago/mainapp/req/AppendServiceReq.java
@@ -0,0 +1,33 @@
+package com.ashago.mainapp.req;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Data
+public class AppendServiceReq {
+    @NotBlank
+    private String category;
+    @NotNull
+    private Boolean isOffLineSupport;
+    @NotBlank
+    private String location;
+    @NotBlank
+    private String title;
+    @NotBlank
+    private String desc;
+    @NotBlank
+    private String serviceProviderId;
+    @NotNull
+    private Boolean isOnlineSupport;
+    @NotBlank
+    private String image;
+    @NotBlank
+    private String detailHtml;
+    @NotBlank
+    private String detailPlaintext;
+    @NotBlank
+    private String price;
+    private Integer priority;
+}
