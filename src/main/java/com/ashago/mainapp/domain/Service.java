7e45da71b04fc67e00597f4afa62a373e1b4252b
huazheng <huazheng@lattebank.com>
2021-03-15 20:25:31 +0800

    service

--- src/main/java/com/ashago/mainapp/domain/Service.java
+++ src/main/java/com/ashago/mainapp/domain/Service.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -23,6 +24,7 @@ public class Service {
     private Boolean isOffLineSupport;
     private String location;
     private String title;
+    @Column(name = "[desc]")
     private String desc;
     private String serviceProviderId;
     private Boolean isOnlineSupport;
